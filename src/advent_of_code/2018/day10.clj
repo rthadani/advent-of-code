d47384dcc8f9138e02d37890a4cc5106980aa2fa
rthadani <rohit.thadani@gmail.com>
2018-12-21 13:00:26 -0800

    day 10

--- src/advent_of_code/2018/day10.clj
+++ src/advent_of_code/2018/day10.clj
@@ -12,10 +12,11 @@
     (str/split-lines)
     read-points))
 
-
 (defn update-point
-  [{:keys [x y v-x v-y]}]
-  {:x (+ x v-x) :y (+ y v-y) :v-x v-x :v-y v-y})
+    [point]
+    (-> point
+        (update :x + (:v-x point))
+        (update :y + (:v-y point))))
 
 (defn area
   [points]
@@ -23,73 +24,42 @@
         min-y (:y (apply min-key :y points))
         max-x (:x (apply max-key :x points))
         max-y (:y (apply max-key :y points))]
-    (Math/abs (* (- min-x max-x) (- min-y max-y)))))
-
+    (* (- max-x min-x) (- max-y min-y))))
 
 (defn create-board
   [points]
   (let [min-x (:x (apply min-key :x points))
         min-y (:y (apply min-key :y points))
         max-x (:x (apply max-key :x points))
-        max-y (:y (apply max-key :y points))] 
-       (println (- max-x min-x) (- max-y min-y))
-       (reduce
-        (fn [board {:keys [x y]}] (assoc-in board [(- y min-y) (- x min-x)] "#"))
-        (vec (repeat (inc (- max-y min-y)) (vec (repeat (inc (- max-x min-x)) "."))))
-        points)))
+        max-y (:y (apply max-key :y points))]
+    (reduce
+     (fn [board {:keys [x y]}] (assoc-in board [(- y min-y) (- x min-x)] "#"))
+     (vec (repeat (inc (- max-y min-y)) (vec (repeat (inc (- max-x min-x)) "."))))
+     points)))
 
 (defn board->string
   [board]
-  (str/join \newline (map #(str/join "" %) board)))
+  (str/join \newline  (map #(str/join "" %) board)))
 
-
-(defn part1
+(defn iterate-board
   [input]
-  (->> (iterate
-        (fn [[points current-area]]
-          (let [new-points (map update-point points)]
-            [new-points (area new-points) current-area]))
-        [input (area input) (inc (area input))])
-       (take-while (fn [[_  a p]]  (< a p)))
-       last
-       first
-       create-board
-       board->string))
+  (->>
+   (iterate
+    (fn [[points points-area _]]
+      (let [new-points (map update-point points)]
+        [new-points (area new-points) points-area]))
+    [input (area input) (inc (area input))])
+   (take-while (fn [[_  a p]]  (< a p)))))
+    
+(def part1
+  (->>  input
+        iterate-board 
+        last
+        first
+        create-board
+        board->string))
 
-(def test-input
-  (read-points 
-   [
-"position=< 9,  1> velocity=< 0,  2>"
-"position=< 7,  0> velocity=<-1,  0>"
-"position=< 3, -2> velocity=<-1,  1>"
-"position=< 6, 10> velocity=<-2, -1>"
-"position=< 2, -4> velocity=< 2,  2>"
-"position=<-6, 10> velocity=< 2, -2>"
-"position=< 1,  8> velocity=< 1, -1>"
-"position=< 1,  7> velocity=< 1,  0>"
-"position=<-3, 11> velocity=< 1, -2>"
-"position=< 7,  6> velocity=<-1, -1>"
-"position=<-2,  3> velocity=< 1,  0>"
-"position=<-4,  3> velocity=< 2,  0>"
-"position=<10, -3> velocity=<-1,  1>"
-"position=< 5, 11> velocity=< 1, -2>"
-"position=< 4,  7> velocity=< 0, -1>"
-"position=< 8, -2> velocity=< 0,  1>"
-"position=<15,  0> velocity=<-2,  0>"
-"position=< 1,  6> velocity=< 1,  0>"
-"position=< 8,  9> velocity=< 0, -1>"
-"position=< 3,  3> velocity=<-1,  1>"
-"position=< 0,  5> velocity=< 0, -1>"
-"position=<-2,  2> velocity=< 2,  0>"
-"position=< 5, -2> velocity=< 1,  2>"
-"position=< 1,  4> velocity=< 2,  1>"
-"position=<-2,  7> velocity=< 2, -2>"
-"position=< 3,  6> velocity=<-1, -1>"
-"position=< 5,  0> velocity=< 1,  0>"
-"position=<-6,  0> velocity=< 2,  0>"
-"position=< 5,  9> velocity=< 1, -2>"
-"position=<14,  7> velocity=<-2,  0>"
-"position=<-3,  6> velocity=< 2, -1>" ]))
-
- (println 
-    (part1 input))
+(def part2
+  (->>  input
+       iterate-board 
+       count))
