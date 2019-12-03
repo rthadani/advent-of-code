d44c41a9a74bfad9e733d5d377fad191472544df
Rohit Thadani <rthadani@plume.com>
2019-12-02 18:50:04 -0800

    added day 1 2019 and specter this time

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.rpl/specter "1.1.3"] ]
   :jvm-opts ["-Xmx4096M"])
---
+++ src/advent_of_code/2019/day1.clj
@@ -0,0 +1,35 @@
+(ns advent-of-code.2019.day1
+  (:require [com.rpl.specter :refer :all]
+            [clojure.string :as str]))
+            
+
+(defn read-input [file]
+  (as-> (slurp file) $
+    (str/split $ #"\n")
+    (transform [ALL] #(Integer/parseInt %) $)))
+
+(def input (read-input "resources/2019/input1"))
+
+(defn fuel [mass]
+  (-> mass (/ 3) (int) (- 2))) 
+
+#_(fuel 1969)
+
+(defn recursive-fuel
+  [mass]
+  (->> (iterate fuel mass)
+       (take-while #(>= % 0))
+       (rest)
+       (apply +)))
+
+#_(recursive-fuel 1969)
+
+(defn fuel-calculator 
+  [massfn]
+  (->> (transform [ALL] massfn input)
+       (apply +)))
+
+;;part1
+(fuel-calculator fuel)
+;;part2
+(fuel-calculator recursive-fuel)
