892b3fa7f5876922c9a631ff7f635a6f2e376006
Rohit Thadani <rthadani@plume.com>
2019-12-08 19:43:42 -0800

    day 8

---
+++ src/advent_of_code/2019/day8.clj
@@ -0,0 +1,35 @@
+(ns advent-of-code.2019.day9)
+
+
+(def input (slurp "resources/2019/input8"))
+
+(defn input->layers [input width height]
+  (->> (partition (* width height) input)
+       #_(map #(partition width %))))
+
+(input->layers "123456789012" 3 2)
+
+(defn layer-with-fewest-zeroes
+ [layers]
+ (->> layers
+      (map #(frequencies %))
+      (apply min-key (fn [f] (get f \0 0)))))
+
+
+(layer-with-fewest-zeroes (input->layers "123456789012" 3 2))
+
+;;part 1
+(apply * (-> (input->layers input 25 6)
+             layer-with-fewest-zeroes
+             (select-keys [\1 \2])
+             vals)) 
+
+;;part 2
+(def layers (input->layers input 25 6))
+ (->> layers
+      (apply interleave)
+      (partition (count layers))
+      (map (fn [pixels] (first (filter #(not= % \2) pixels))))
+      (apply str)
+      (map #(if (= % \1) "#" " "))
+      (partition 25))
