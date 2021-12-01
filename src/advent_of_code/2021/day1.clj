7b8e6e5d8515581b6e9a05d721050753f098e5ab
rthadani <rohit.thadani@gmail.com>
2021-12-01 09:28:09 -0800

    Advent of code 2021 day 1

---
+++ src/advent_of_code/2021/day1.clj
@@ -0,0 +1,21 @@
+(ns advent-of-code.2021.day1
+  (:require [clojure.string :as str]))
+
+(defn read-input [file]
+  (as-> (slurp file) $
+    (str/split $ #"\n")
+    (map #(Integer/parseInt %) $)))
+
+;;day1.1
+(->> (read-input "resources/2021/input1")
+     (partition 2 1)
+     (filter (fn [[f s]] (> s f)))
+     count)
+
+;;day 1.2
+(->> (read-input "resources/2021/input1")
+     (partition 3 1)
+     (map #(apply + %))
+     (partition 2 1)
+     (filter (fn [[f s]] (> s f)))
+     count)
