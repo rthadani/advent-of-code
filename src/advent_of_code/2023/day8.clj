64e682fbd967b6372d1fcbce678ae2ecb31c0f3f
rthadani <rohit.thadani@gmail.com>
2023-12-11 14:32:16 -0800

    2023 day 8

--- src/advent_of_code/2023/day8.clj
+++ src/advent_of_code/2023/day8.clj
@@ -8,22 +8,40 @@
         nodes (->> nodes 
                    (str/split-lines) 
                    (reduce (fn [n l] 
-                             (let [dirs (re-seq #"[A-Z]+" l)]
+                             (let [dirs (re-seq #"[A-Z\d+]+" l)]
                                (conj n [(first dirs) (vec (rest dirs))]))) 
-                           []))]
+                           [])
+                  (into {}))]
     [direction nodes]))
 
-(defn get-val
-  [nodes key]
-  (->> (filter #(= (first %) key) nodes)
-       second))
+(defn travel
+  [directions nodes starting-pos]
+   (reductions (fn [curr dir]
+                 (if (= dir \L) 
+                   (first (nodes curr))
+                   (second (nodes curr))))
+               starting-pos
+               (cycle directions)))
+
+(defn step-count
+  [start directions end-when nodes]
+  (->> start 
+       (travel directions nodes)
+       (take-while end-when)
+       count))
+;;part 1
+(let [[d n] (read-input "resources/2023/input8")]
+  (step-count "AAA" d (complement #{"ZZZ"}) n)) ; 18673
+
+;;part-2
+(defn lcm
+  [a n]
+  (letfn [(gcd [a b] (if (zero? b) a (gcd b (mod a b))))]
+          (/ (* a n) (gcd a n))))
 
 (let [[d n] (read-input "resources/2023/input8")]
-  (reduce 
-    (fn [[k i] d]
-      (if (= k "ZZZ")
-        (reduced i)
-       (case d
-        \L [ (inc i)])))
-    [(ffirst n) 0]
-    d))
+  (->> (keys n)
+       (filter (comp #{\A} last))
+       (map (fn [s] (step-count s d (comp (complement #{\Z}) last) n)))
+       (reduce lcm 1)) ) ; 17972669116327
+
